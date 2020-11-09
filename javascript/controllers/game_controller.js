47ac07f202b5c9098520d1e49a3ef648de383784
PY <pyves.lg@gmail.com>
2020-11-09 17:43:26 +0100

    countdown to lose points

--- javascript/controllers/game_controller.js
+++ javascript/controllers/game_controller.js
@@ -2,13 +2,56 @@ import { Controller } from "stimulus";
 import Swal from 'sweetalert2';
 
 export default class extends Controller {
-	static targets = ['mainContainer', 'playerList', 'numPlayers', 'letterContainer', 'categoryContainer', 'player']
+	static targets = ['mainContainer', 'playerList', 'numPlayers', 'letterContainer', 'categoryContainer', 'player', 'launcher']
 
   connect() {
     const letters = [...Array(26)].map((q,w)=>String.fromCharCode(w+97).toUpperCase());
     this.letters = letters;
     const categories = ["Ville Française", "Ville Étrangère", "Pays", "Animal", "Prénom Masculin", "Prénom Féminin", "Profession", "Arbre", "Fleur", "Sport", "Chanteur", "Chanteuse", "Acteur", "Actrice", "Couleur", "Fleuve ou Rivière", "Athlète de haut niveau", "Auteur", "Marque", "Héros", "Fruit", "Peintre", "Personnalité politique"]
     this.categories = categories
+    let countdown;
+  	this.countdown = countdown;
+
+  }
+
+  timer(seconds) {
+  	const displayTime = (seconds) => {
+  		if (seconds == 0) {
+  			this.launcherTarget.innerHTML = `Terminé ! 😵`;
+	  		Swal.fire({
+	  		  title: `Temps dépassé ! 😵`,
+	  		  text: `
+	  		  	Chaque joueur ayant des points perd 1 point ! 🤕
+	  		  `,
+	  		  confirmButtonText: `Oh nooooon ! 🤯`
+	  		}).then((result) => {
+			  /* Read more about isConfirmed, isDenied below */
+				  if (result.isConfirmed) {
+				  	this.playerTargets.forEach((player) => {
+				  		const score = parseInt(player.dataset.score, 10);
+				  		const new_score = (score === 0 ) ? 0 : (score - 1);
+				  		player.dataset.score = new_score;
+				  		player.innerHTML = `${player.dataset.name} <span class="points">${new_score}</span>`;
+				  	})
+				  	this.launcherTarget.innerHTML = `Prêt(e)? Prêt(e) ! 😊`;
+				  }
+			  });
+
+  		} else {
+  			this.launcherTarget.innerHTML = `Plus que <b>${seconds >= 10 ? seconds : `0${seconds}`}</b> secondes! 😱`;
+  		}
+  	}
+		const now = Date.now();
+		const then = now + seconds * 1000;
+		displayTime(seconds);
+		this.countdown = setInterval(() => {
+			const secondsLeft = Math.round((then - Date.now()) / 1000);
+			if (secondsLeft < 0) {
+				clearInterval(this.countdown);
+				return;
+			};
+			displayTime(secondsLeft);
+		},1000)
   }
 
   async focus() {
@@ -34,18 +77,23 @@ export default class extends Controller {
   addPlayer() {
   	const playerName = document.getElementById('player').value;
   	const numberOfPlayer = this.playerListTarget.querySelectorAll('li').length;
-  	this.playerListTarget.insertAdjacentHTML('beforeend', `<li data-name="${playerName}" data-score="0" data-target="game.player" data-id="${numberOfPlayer + 1}" class="list-inline-item" data-action="click->game#addPoint">${playerName}</li>`);
+  	this.playerListTarget.insertAdjacentHTML('beforeend', `<li data-name="${playerName}" data-score="0" data-target="game.player" data-id="${numberOfPlayer + 1}" class="list-inline-item" data-action="click->game#addPoint">${playerName}<span class="points">0</span></li>`);
   	this.mainContainerTarget.innerHTML = "";
   	this.numPlayersTarget.innerHTML = `<b>${numberOfPlayer + 1}</b> Joueurs`;
   }
 
   addPoint() {
+  	clearInterval(this.countdown);
   	const player = event.currentTarget;
   	const score = parseInt(player.dataset.score, 10);
   	const name = player.dataset.name;
   	player.dataset.score = score + 1;
   	player.innerHTML = `${name} <span class="points">${score + 1}</span>`;
   	setTimeout(this.checkWinner(player), 1000) ;
+  	this.launcherTarget.innerHTML = `Bravo ${name} ! 👏`;
+  	setTimeout(() => {
+  		this.launcherTarget.innerHTML = `Prêt(e)? Prêt(e) ! 😊`;
+  	}, 2000)
   }
 
   computeScores() {
@@ -53,19 +101,24 @@ export default class extends Controller {
 
   }
 
-  sentence(obj) {
-  	return `<li>${obj.name}: ${obj.score}</li>`;
-  }
   checkWinner(player) {
   	if (parseInt(player.dataset.score) == 10 ) {
-  		const array = this.computeScores();
+  		const array = this.computeScores().sort((a, b) => b.score - a.score);
   		Swal.fire({
-  		  title: `🎉🎉🎉 ${player.dataset.name} a gagné !! 🎉🎉🎉`,
-  		  text: `
-  		  	Les scores ici... bientôt ! Mais là je dois jouer avec ma Louloutte d'Amour que j'aime fort ❤️
+  		  title: `🥇 ${player.dataset.name} gagne ! 🥇`,
+  		  html: `
+  		  	<ol>${array.map(player => `<li>${player.name}: ${player.score}</li>`).join('')}</ol>
   		  `,
-  		  confirmButtonText: 'Trop fort ! 👏'
-  		})
+  		  confirmButtonText: 'Nouvelle partie'
+  		}).then((result) => {
+		  /* Read more about isConfirmed, isDenied below */
+			  if (result.isConfirmed) {
+			  	this.playerTargets.forEach((player) => {
+			  		player.dataset.score = 0;
+			  		player.innerHTML = `${player.dataset.name}`;
+			  	})
+			  }
+		  });
   	}
   }
 
@@ -96,6 +149,7 @@ export default class extends Controller {
   		this.letterContainerTarget.classList.add('animate__bounce');
   		setTimeout(() => {
   			this.randomCategory();
+  			this.timer(20);
   		}, 1000)
   	}, 2000);
   }
